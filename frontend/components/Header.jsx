d5802591348d45e43369e153bfad0b7414f90b71
mAlex28 <alexthegeek2001@gmail.com>
2026-07-19 04:32:38 +0100

    changed logo

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,10 +1,17 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
     <div className="tt-topbar">
       <div className="tt-brand">
-        <div className="tt-brand-mark">
-          <div className="tt-brand-mark-dot" />
-        </div>
+        <Image
+          src="/logo.png"
+          alt="TrackAndTrail logo"
+          width={354}
+          height={220}
+          priority
+          style={{ height: 34, width: "auto" }}
+        />
         <div className="tt-brand-word">
           Track<em>&amp;</em>Trail
         </div>
